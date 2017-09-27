b0fd4d50cc6ccd8c27cc28f9b05de9a6eea1e280
Long Nguyen <zakelong81@github.com>
2017-09-27 10:25:30 -0700

    add upercase and make it clean

--- open_read_write/main.c
+++ open_read_write/main.c
@@ -7,53 +7,32 @@
 #include <ctype.h>
 
 #define BUF_SIZE 8192
-
 int main(int argc, char* argv[]) {
 
     int input_fd, output_fd;    /* Input and output file descriptors */
-    ssize_t ret_in, ret_out;    /* Number of bytes returned by read() and write() */
+    ssize_t ret_in;    /* Number of bytes returned by read() and write() */
     char buffer[BUF_SIZE];      /* Character buffer */
+    char lower_to_upper[BUF_SIZE];
 
-    /* Are src and dest file name arguments missing
-    if(argc != 3){
-        printf ("Usage: cp file1 file2");
-        return 1;
-    }
 
-    */
-    /* Create input file descriptor */
-    input_fd = open (argv [1], O_RDONLY);
-    if (input_fd == -1) {
-            perror ("open");
-            return 2;
-    }
-
+    /* open a file*/
+    if (argv [1] == NULL)
+        input_fd = open (some_file, O_RDONLY);
+    else
+        input_fd = open (argv [1], O_RDONLY);
     /* Create output file descriptor */
-    output_fd = open("out.txt", O_WRONLY | O_CREAT, 0644);
-    if(output_fd == -1){
-        perror("open");
-        return 3;
-    }
-
+    if (argv [2] == NULL)
+        output_fd = open("some_file.up", O_WRONLY | O_CREAT, 0644);
+    else
+        output_fd = open(argv [2], O_WRONLY | O_CREAT, 0644);
 
     /* Copy process */
-    char tmp[BUF_SIZE];
-    int i = 0;
     while((ret_in = read (input_fd, &buffer, BUF_SIZE)) > 0)
     {
-      printf ("%zu\n", ret_in);
-      while(buffer[i])
-      {
-        tmp[i] = toupper(buffer[i]);
-        i++;
-      }
-      ret_out = write (output_fd, &tmp, (ssize_t) ret_in);
-
-      if(ret_out != ret_in){
-      /* Write error */
-          perror("write");
-          return 4;
-        }
+      printf ("%zu\n", ret_in); // printout the packet size in bytes
+      for (int i = 0 ; buffer[i]; i ++)
+        lower_to_upper[i] = toupper(buffer[i]);
+      write (output_fd, &lower_to_upper, (ssize_t) ret_in);
     }
 
     /* Close file descriptors */
